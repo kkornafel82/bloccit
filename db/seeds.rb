44a490fdf43c9cf8ddadbac3283507d3d2f6e180
Kevin <kkornafel82@yahoo.com>
2015-09-05 19:34:37 -0700

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,60 @@
-require 'faker'
 
-# Create Posts
-50.times do 
-  Post.create!(
-   title: Faker::Lorem.sentence,
-   body:  Faker::Lorem.paragraph
-    )
-end
+#Post.all.each do |post|
+ #title = post.title
+#if title == "Newest"
 
-posts = Post.all
+  #nil
+#else Post.create!(title: "Newest Title", body: "Newest Body")
+#end
+#end
 
-#Create Comments
-100.times do
-  Comment.create!(
-   post: posts.sample,
-   body: Faker::Lorem.paragraph
-   )
-end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+
+
+#Post.all.fetch ("New")
+ # if nil
+  #  Post.create!(title: "New", body: "New")
+  #else
+
+
+
+   #Post.has_value?(title: "New")
+    #if false
+     # Post.create!(title: "New", body: "New")
+    #else 
+     # puts "Already exists"
+    #end
+
+    #Comment.has_value?("New Comment")
+    #if nil
+     # Comment.create!(body: "New Comment")
+    #else 
+     # puts "Already"
+    #end
+
+
+   # puts "Created a new post"
+  #else 
+   # puts "already exists"
+  
+
+
+
+#Comment.all.select {|comment| comment.body = "Newest Body" }
+#if nil 
+ # puts "nothing there"
+#else 
+ # puts "Something there"
+#end
+ # if nil
+  #  Post.comment.create!(body: "New")
+   # pust "created a new comment"
+  #else 
+   # puts "already exists"
+  #end
+
+  post = {title: "Newest Title", body: "Newest Body"}
+  Post.create(post) unless Post.where(post).first
+
+  comment = {body: "Newest Comment Body"}
+  Comment.create(comment) unless Comment.where(comment).first
