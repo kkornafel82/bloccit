a668850bffe7719ed73afac5e65ea7564278b23f
Kevin <kkornafel82@yahoo.com>
2015-09-07 21:23:25 -0700

    Checkpoint 36 completed, User Authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,12 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
