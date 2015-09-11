ff4c12ee4c29642fe2db60f6c3cb3ea2e88974c8
Kevin <kkornafel82@yahoo.com>
2015-09-10 21:20:16 -0700

    Checkpoint 39 completed, authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,12 +40,46 @@ posts = Post.all
    )
 end
 
-user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'noel1104@yahoo.com',
-   password: 'terrapin'
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save!
+ 
+ # Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
  )
+moderator.skip_confirmation!
+moderator.save!
+ 
+ # Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
+member.skip_confirmation!
+member.save!
+
+
+
+
+
+
+#user = User.first
+ #user.skip_reconfirmation!
+ #user.update_attributes!(
+  # email: 'noel1104@yahoo.com',
+  # password: 'terrapin'
+ #)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
