291645454a2d9267f5e5b1a39f316bd0c2a78bab
Kevin <kkornafel82@yahoo.com>
2015-09-13 14:08:12 -0700

    Reconfigured app for Production emails, 6th try

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,27 @@
 require 'faker'
 
-# Create Users
- 5.times do
-   user = User.new(
-     name:     Faker::Name.name,
-     email:    Faker::Internet.email,
-     password: Faker::Lorem.characters(10)
-   )
-   user.skip_confirmation!
-   user.save!
- end
+ #Create Users
+  
+ 1.times do 
+  user = User.new(
+    name:   "Kevin",
+    email:  "kkornafel82@yahoo.com",
+    password: "terrapin"
+    )
+
+  user.skip_confirmation!
+  user.save!
+end
+
+ #1.times do
+  # user = User.new(
+    # name:     Faker::Name.name,
+     #email:    Faker::Internet.email,
+     #password: Faker::Lorem.characters(10)
+  # )
+   #user.skip_confirmation!
+   #user.save!
+ #end
  users = User.all
 
  # Create Topics
