d3b6c63c85c5e68744ecaf889885d928770d8fbf
Kevin <kkornafel82@yahoo.com>
2015-09-15 18:01:29 -0700

    post markdown progress

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,9 +64,12 @@ class PostsController < ApplicationController
   private
 
   def render_as_markdown
-    markdown_to_html
-    end
-
+  renderer = Redcarpet::Render::HTML.new
+  extensions = {fenced_code_blocks: true}
+  redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+  (redcarpet.render markdown).html_safe
+  end
+  
   def post_params
    params.require(:post).permit(:title, :body)
   end
