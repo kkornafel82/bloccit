1ac81d860eceee1e22bdf6fdc549332e4d6028b2
Kevin <kkornafel82@yahoo.com>
2015-09-23 20:47:30 -0700

    checkpoint 51 completed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post: post)
-
+    authorize favorite
     if favorite.save
       flash[:notice] = "Post was marked as Favorite!"
       redirect_to [post.topic, post]
@@ -12,7 +12,20 @@ class FavoritesController < ApplicationController
       redirect_to [post.topic, post]
     end
   end
-  
+
+  def destroy
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+    if favorite.destroy
+       flash[:notice] = "Favorite was deleted successfully."
+       redirect_to [post.topic, post]
+     else
+       flash[:error] = "There was an error deleting the favorite."
+       redirect_to [post.topic, post]
+     end
+   end
+
 
 
 
