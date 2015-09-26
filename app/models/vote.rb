82122fff6c23c0872f6baead0cf63a83577f0d97
Kevin <kkornafel82@yahoo.com>
2015-09-25 18:29:16 -0700

    checkpoint 57 progress

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,18 @@ class Vote < ActiveRecord::Base
 
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
 
+  def up_vote?
+    value == 1
+  end
+ 
+  def down_vote?
+    value == -1
+  end
+
   after_save :update_post
+
+  
+
 
   private
 
